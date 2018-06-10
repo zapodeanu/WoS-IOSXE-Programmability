e5a0576c25906c0260d7aa540825b8c0c464995e
gzapodea <gzapodea@cisco.com>
2018-06-10 18:53:40 -0400

    uploading for CL US Orlando

---
+++ config.py
@@ -0,0 +1,29 @@
+
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+
+# This file contains the Spark Auth, DNAC user name and password, ServiceNow developer instance info
+
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'NzRiZGU0M2UtOWJjNy00MzQwL....'
+SPARK_ROOM = 'IT Personal Assistant'  # to be used when manager arrives to office
+
+
+DNAC_URL = 'https://ip_address'
+DNAC_USER = 'admin'
+DNAC_PASS = 'password'
+
+
+SNOW_URL = 'https://devxxxxx.service-now.com/api/now'
+SNOW_ADMIN = 'admin'
+SNOW_DEV = 'IOSXE'
+SNOW_PASS = 'password'
+SNOW_INSTANCE = 'devxxxx'
+
+DEV_USER = 'user'
+DEV_PASSW = 'password'
+DEV_IP = 'ip_address'
+
+UNAUTH_USER = 'orlando'
